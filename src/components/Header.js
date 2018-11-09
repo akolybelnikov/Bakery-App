ece0457b7ac12a25025e8bff320039935e294d3f
akolybelnikov <a.kolybelnikov@gmail.com>
2018-11-09 23:48:56 +0100

    responsive styling of the header elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
-import React, {PureComponent} from 'react';
+import React, {PureComponent, Fragment} from 'react';
 import {
     Navbar,
     NavbarBrand,
-    NavbarBurger,
     NavbarItem,
     NavbarEnd,
     Image,
     Field,
     Control,
     Icon,
-    Input
+    Input,
+    Button
 } from 'bloomer'
 import styled from 'styled-components'
 import logo from '../assets/logos/logo-sm.png'
+import MediaQuery from 'react-responsive';
 
 const StyledNavbar = styled(Navbar)`
     position: fixed;
@@ -24,10 +25,11 @@ const StyledNavbar = styled(Navbar)`
 const HeaderImage = styled(Image)`
     height: 80px;
     width: 150px;
+    transition: all 1s ease-in-out;
     @media screen and (min-width: 769px) {
         margin-left: 1rem;
     }
-    @media screen and (max-width: 768px) {
+    @media screen and (max-width: 825px) {
         height: 70px;
         width: 130px;
     }
@@ -35,25 +37,29 @@ const HeaderImage = styled(Image)`
         height: 60px;
         width: 112.5px;
     }
+    @media screen and (max-width: 568px) {
+        height: 48px;
+        width: 90px;
+    }
     @media screen and (max-width: 320px) {
         height: 35px;
         width: 65px;
     }
 `
-const StyledNavbarBurger = styled(NavbarBurger)`
-    :hover {
-        background-color: #fff;
+const StyledNavbarItem = styled(NavbarItem)`
+    flex-grow: 1;
+    justify-content: flex-end;
+    span.icon {
+        transition: all 1s ease-in-out;
     }
-    height: 4.25rem;
-    width: 4.25rem;
-    @media screen and (max-width: 321px) {
-        height: 3.25rem;
-        width: 3.25rem;
+    @media screen and (max-width: 320px) {
+        padding: 0.5rem;
     }
 `
 const HeaderControl = styled(Control)`
     span.icon {
         color: ${props => props.theme.success}!important;
+        transition: all 1s ease-in-out;
     }
 }
 `
@@ -80,19 +86,85 @@ export default class Header extends PureComponent {
                     <NavbarItem>
                         <HeaderImage src={logo}/>
                     </NavbarItem>
-                    <NavbarItem isHidden="desktop">
+                    <StyledNavbarItem isHidden="desktop">
                         <Field>
                             <HeaderControl hasIcons="left">
-                                <Input
-                                    placeholder="поиск по сайту"
-                                    type="text"
-                                    isColor="success"
-                                    isSize="small"/>
-                                <Icon isSize='small' isAlign='left' className="fas fa-search"></Icon>
+                                <MediaQuery minDeviceWidth={736}>
+                                    {(matches) => {
+                                        if (matches) {
+                                            return (
+                                                <Fragment>
+                                                    <Input
+                                                        placeholder="поиск по сайту"
+                                                        type="text"
+                                                        isColor="success"
+                                                        isSize="large"/>
+                                                    <Icon isSize='large' isAlign='left' className="fas fa-search"></Icon>
+                                                </Fragment>
+                                            )
+                                        } else {
+                                            return (
+                                                <Fragment>
+                                                    <Input
+                                                        placeholder="поиск по сайту"
+                                                        type="text"
+                                                        isColor="success"
+                                                        isSize="small"/>
+                                                    <Icon isSize='small' isAlign='left' className="fas fa-search"></Icon>
+                                                </Fragment>
+                                            )
+                                        }
+                                    }}
+                                </MediaQuery>
                             </HeaderControl>
                         </Field>
-                    </NavbarItem>
-                    <StyledNavbarBurger isActive={this.state.isActive} onClick={this.onClickNav}/>
+                    </StyledNavbarItem>
+                    <StyledNavbarItem isHidden="desktop">
+                        {!this.state.isActive && <MediaQuery minDeviceWidth={736}>
+                            {(matches) => {
+                                if (matches) {
+                                    return (
+                                        <Fragment>
+                                            <Button isSize="large" onClick={this.onClickNav}>
+                                                <Icon className="fas fa-bars"></Icon>
+                                            </Button>
+                                        </Fragment>
+                                    )
+                                } else {
+                                    return (
+                                        <Fragment>
+                                            <Button isSize="small" onClick={this.onClickNav}>
+                                                <Icon className="fas fa-bars"></Icon>
+                                            </Button>
+                                        </Fragment>
+                                    )
+                                }
+                            }}
+                        </MediaQuery>
+}
+                        {this.state.isActive && <MediaQuery minDeviceWidth={736}>
+                            {(matches) => {
+                                if (matches) {
+                                    return (
+                                        <Fragment>
+                                            <Button isSize="large" onClick={this.onClickNav}>
+                                                <Icon className="fas fa-times"></Icon>
+                                            </Button>
+                                        </Fragment>
+                                    )
+                                } else {
+                                    return (
+                                        <Fragment>
+                                            <Button isSize="small" onClick={this.onClickNav}>
+                                                <Icon className="fas fa-times"></Icon>
+                                            </Button>
+                                        </Fragment>
+                                    )
+                                }
+                            }}
+                        </MediaQuery>
+}
+                    </StyledNavbarItem>
                 </NavbarBrand>
                 <NavbarEnd>
                     <NavbarItem isHidden="touch">
@@ -102,8 +174,8 @@ export default class Header extends PureComponent {
                                     placeholder="поиск по сайту"
                                     type="text"
                                     isColor="success"
-                                    isSize="medium"/>
-                                <Icon isSize='medium' isAlign='left' className="fas fa-search"></Icon>
+                                    isSize="large"/>
+                                <Icon isSize='large' isAlign='left' className="fas fa-search"></Icon>
                             </HeaderControl>
                         </Field>
                     </NavbarItem>
